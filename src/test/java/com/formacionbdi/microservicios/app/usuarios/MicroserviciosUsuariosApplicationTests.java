eed00ced4d45fe23b6b9c6d37952c97b471080cb
japvidal <japvidal@gmail.com>
2026-03-31 11:25:33 +0200

    Fix SpringBootTest configuration for futfem service tests

--- src/test/java/com/formacionbdi/microservicios/app/usuarios/MicroserviciosUsuariosApplicationTests.java
+++ src/test/java/com/formacionbdi/microservicios/app/usuarios/MicroserviciosUsuariosApplicationTests.java
@@ -1,17 +1,15 @@
-package com.formacionbdi.microservicios.app.usuarios;
+package com.microservicios.app.futfem.squads;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = MicroserviciosFutfemSquadsTempApplication.class)
 @ActiveProfiles("test")
 class MicroserviciosUsuariosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
-
-
